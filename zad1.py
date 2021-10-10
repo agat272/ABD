a41d067fa1aa085c1e578bc661ae1370197435fc
agat272 <aswatow@student.agh.edu.pl>
2021-10-10 12:44:26 +0200

    zadanie 3

--- zad1.py
+++ zad1.py
@@ -1,3 +1,4 @@
+#Agata Swatowska, zadanie 3- rysowanie wykresow funkcji x^2+5
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,15 +9,28 @@ x1 = np.linspace(-1,1)
 x2 = np.linspace(-6,6)
 x3 = np.linspace(0,5)
 
-plt.subplot(3,1,1)
-plt.plot(x1,function(x1))
+fig, ax = plt.subplots(3,1)
+fig.set_size_inches(6, 18)
 
+y1 = function(x1)
+ax[0].plot(x1, y1, label = 'x^2+5')
+ax[0].set_ylabel("y")
+ax[0].legend()
+ax[0].set_title("wykres (-1, 1)")
+ax[0].grid()
 
-plt.subplot(3,1,2)
-plt.plot(x2,function(x2))
+ax[1].plot(x2,function(x2), label = 'x^2+5')
+ax[1].set_title("wykres (-6, 6)")
+ax[1].set_ylabel("y")
+ax[1].legend()
+ax[1].grid()
 
+ax[2].plot(x3,function(x3), label = 'x^2+5')
+ax[2].set_title("wykres (0, 5)")
+ax[2].set_ylabel("y")
+ax[2].legend()
+ax[2].grid()
 
-plt.subplot(3,1,3)
-plt.plot(x3,function(x3))
-
+plt.xlabel("x")
 plt.show()
+fig.savefig("wyk1.jpg")
